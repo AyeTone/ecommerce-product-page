aebfd916cf5ca72fd1952237b7418e98d912aca6
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-06-13 20:12:27 -0400

    Refactored Navbar Component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,22 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { ReactComponent as Menu } from "../svgs/icon-menu.svg";
 import { ReactComponent as Cart } from "../svgs/icon-cart.svg";
 import { ReactComponent as Close } from "../svgs/icon-close.svg";
 import Avatar from "../images/image-avatar.png";
 import Context from "../Context/Context";
+// import { useState } from "react";
 
 const Navbar = () => {
   const { setShowCart, cart } = useContext(Context);
+  const navItems = ["Collections", "Men", "Women", "About", "Contact"];
+
+  const displayNavItems = navItems.map((item, id) => {
+    return (
+      <li className="navbar__list--item" key={id}>
+        {item}
+      </li>
+    );
+  });
 
   function showMenu() {
     let dropdown = document.getElementById("dropdown") as HTMLDivElement;
@@ -28,45 +38,31 @@ const Navbar = () => {
     setShowCart((prev: boolean) => !prev);
   }
 
-  const [width, setWidth] = useState(0);
+  // const [width, setWidth] = useState(0);
 
-  window.onresize = () => {
-    setWidth(window.screen.width);
-    if (width > 1020) {
-      closeMenu();
-    }
-  };
+  // window.onresize = () => {
+  //   setWidth(window.screen.width);
+  //   if (width >= 1020) {
+  //     closeMenu();
+  //   }
+  // };
 
   return (
     <nav className="navbar">
       <div className="navbar__left">
         <h1 className="navbar__left--title">sneakers</h1>
-        <Menu onClick={() => showMenu()} className="navbar__menu--btn" />
-        <div id="dropdown" className="navbar__menu">
+        <Menu onClick={() => showMenu()} className="navbar__dropdown--btn" />
+        <div id="dropdown" className="navbar__dropdown">
           <ul className="navbar__list">
             <Close
               onClick={() => closeMenu()}
               className="navbar__list--item close"
             />
-            <li className="navbar__list--item">Collections</li>
-            <li className="navbar__list--item">Men</li>
-            <li className="navbar__list--item">Women</li>
-            <li className="navbar__list--item">About</li>
-            <li className="navbar__list--item">Contact</li>
+            {displayNavItems}
           </ul>
         </div>
         <div className="navbar__tablet">
-          <ul className="navbar__list">
-            <Close
-              onClick={() => closeMenu()}
-              className="navbar__list--item close"
-            />
-            <li className="navbar__list--item">Collections</li>
-            <li className="navbar__list--item">Men</li>
-            <li className="navbar__list--item">Women</li>
-            <li className="navbar__list--item">About</li>
-            <li className="navbar__list--item">Contact</li>
-          </ul>
+          <ul className="navbar__list">{displayNavItems}</ul>
         </div>
       </div>
 
